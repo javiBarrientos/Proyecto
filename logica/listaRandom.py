ac29ac9e8e049408be3584081aee6a60d76a0bf5
samuFerragut <sferragut@cifpfbmoll.eu>
2019-11-18 20:23:35 +0100

    feat: Crear lista random
    En este archivo queremos crear una lista de canciones con el diccionario
    del acceso a datos

--- logica/listaRandom.py
+++ logica/listaRandom.py
@@ -1,1 +1,15 @@
-from random import random, randint
+from random import random
+from ../accesoDatos/diccionario import x
+
+
+playlist = []
+
+
+def listaRandom(canciones):
+    i = 0
+    while i < 50:
+        cancion = random.choice(libreria.keys())
+        if cancion not in playlist:
+            playlist.append(cancion)
+            i = i+1
+    return playlist
