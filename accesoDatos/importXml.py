56272428fcab152357f102b5b010796c8c20089a
javiBarrientos <jbarrientos@cifpfbmoll.eu>
2019-11-21 19:44:06 +0100

    refactor: por lo menos saca las canciones

--- accesoDatos/importXml.py
+++ accesoDatos/importXml.py
@@ -2,21 +2,7 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('C:/Users/Javi/Desktop/proyecto/accesoDatos/xmlSecundario.xml')
 root = tree.getroot()
 
-# barricada para comprobar que entra
 
-
-def nombresCanciones():
-
-    listaNombreCanciones = []
-
-    for tracks in root:  # child in root
-        for track in tracks:  # subchild in child
-            for name in track.findall('name'):  # name in subchild
-                nombre = track.get('name')
-
-                if nombre not in listaNombreCanciones:
-                    listaNombreCanciones.append(nombre)
-
-# barricada para comprobar que sale
-
-    return listaNombreCanciones
+for tracks in root:
+    for track in tracks:
+        print(track[0].text)
