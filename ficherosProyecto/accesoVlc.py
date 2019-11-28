d5a4a0a4d096dec6ad0d8319131c3c35cc738289
samuFerragut <sferragut@cifpfbmoll.eu>
2019-11-28 20:16:36 +0100

    feat(accesoVlc, main): Abrir VLC

--- ficherosProyecto/accesoVlc.py
+++ ficherosProyecto/accesoVlc.py
@@ -1,11 +1,5 @@
-import os
 import subprocess
-from proyectoVLC import rutaVLC, rutaListaMusica
 
-# canciones tiene que ser la lista aleatoria
-canciones = rutaListaMusica()
 
-# comprobar otro path para program files x86
-vlc = rutaVLC()
-
-subprocess.Popen([vlc, canciones])
+def accederVlc(ruta, lista):
+    subprocess.Popen([ruta, lista])
--- ficherosProyecto/main.py
+++ ficherosProyecto/main.py
@@ -1,12 +1,14 @@
 from importarXml import parsearXml
 from listaDeCanciones import listaDeCanciones
+from accesoVlc import accederVlc
 
 
 def main():
     rutaXml = 'libreriasXml/musicaProyecto.xml'
+    rutaVlc = 'C:/Program Files/VideoLAN/VLC/vlc.exe'
     data = parsearXml(rutaXml)
     cancionesAleatorias = listaDeCanciones(data)
-    print(cancionesAleatorias)
+    accesoVlc = accederVlc(rutaVlc, cancionesAleatorias)
 
 
 main()
