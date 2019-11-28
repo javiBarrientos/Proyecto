e65c39f8656447d88a0f93e19f35584cbb5d21b3
samuFerragut <sferragut@cifpfbmoll.eu>
2019-11-28 21:19:21 +0100

    refactor(main, accesoVlc, listaDeCanciones): Devolver string

--- ficherosProyecto/listaDeCanciones.py
+++ ficherosProyecto/listaDeCanciones.py
@@ -4,10 +4,11 @@ import random
 def listaDeCanciones(diccionario):
     i = 0
     lista = []
+    comilla = '\"'
     while i < len(diccionario):
         for cancion in diccionario:
             cancion = random.choice(list(diccionario.keys()))
             if cancion not in lista:
                 lista.append(cancion)
                 i += 1
-    return lista
+    return ' '.join(lista)
