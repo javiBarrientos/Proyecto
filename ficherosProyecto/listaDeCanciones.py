574cdb21091500d4eb7df755b81772b081becd20
javiBarrientos <jbarrientos@cifpfbmoll.eu>
2019-11-29 18:01:16 +0100

    refactor: Crear string con join

--- ficherosProyecto/listaDeCanciones.py
+++ ficherosProyecto/listaDeCanciones.py
@@ -4,11 +4,15 @@ import random
 def listaDeCanciones(diccionario):
     contador = 0
     lista = []
+
     while contador < len(diccionario):
+
         for cancion in diccionario:
             cancion = random.choice(list(diccionario.keys()))
+
             if cancion not in lista:
                 lista.append(cancion)
                 contador += 1
 
-    return ' '.join(lista)
+    listaToString = ' '.join(lista)
+    return listaToString
