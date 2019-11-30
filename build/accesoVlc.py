580f7ff40de341bd0353cc07641b4ce707ffd5c5
javiBarrientos <jbarrientos@cifpfbmoll.eu>
2019-11-30 19:22:02 +0100

    refactor: Cambiar try except

--- build/accesoVlc.py
+++ build/accesoVlc.py
@@ -3,7 +3,7 @@ import os
 
 def accederVlc(rutaVlc, lista):
     try:
-        f = exec(rutaVlc)
+        exec(rutaVlc)
     except OSError:
         print('El sistema no puede encontrar la ruta especificada')
     else:
