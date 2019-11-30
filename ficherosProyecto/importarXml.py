88a0fa5e695d3f1ed0caf4b695a5d481052e6f90
javiBarrientos <jbarrientos@cifpfbmoll.eu>
2019-11-30 09:52:01 +0100

    feat: Añadir barricada xml

--- ficherosProyecto/importarXml.py
+++ ficherosProyecto/importarXml.py
@@ -5,6 +5,13 @@ def parsearXml(rutaXml):
     tree = ET.parse(rutaXml)
     root = tree.getroot()
 
+    try:
+        f = open(rutaXml)
+    except FileNotFoundError:
+        print('Este fichero no existe')
+    else:
+        pass
+
     diccionario = {}
 
     for tracks in root.iter('track'):
